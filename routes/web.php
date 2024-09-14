24f9d8e6100c65651f4fd83bc4af08faf95e35f6
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-09-14 09:09:01 +0700

    Add tailwind, add routing, add middleware check token

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('welcome');
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home')->middleware('checkToken');
+
+Route::get('/food/{id}', function ($id) {
+    return view('detailFood', ["id" => $id ]);
+})->name('detailFood');
