81042a59b3bf7c83cf64800a13f8f0d2d49b2a07
Ahmad Hilman Dani <ahmadhilmanlagi@gmail.com>
2024-09-20 15:13:28 +0700

    Add table reservation migrations

---
+++ database/migrations/2024_09_18_235251_create_reservations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('restaurant_name');
+            $table->string('restaurant_picture_id');
+            $table->string('restaurant_address');
+            $table->string('reserved_chair');
+            $table->date('rsvp_date');
+            $table->time('rsvp_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
